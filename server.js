e4c9e69aca37e221e9bbe0b4623809e38def37c0
RohitKumarPatel2004 <rohitpatel843332@gmail.com>
2024-04-13 16:54:04 +0530

    change port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
 
